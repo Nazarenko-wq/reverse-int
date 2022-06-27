3728164554a046e1098c715b3dfcf9953e69997d
Nazarenko Michael <mihaaaaa435@gmail.com>
2022-06-27 09:40:21 +0300

    feat did task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+    if(n < 0){
+        let str1 = n * -1;
+        let str = String(str1);
+        let result = str.split('').reverse().join('');
+        return +result;
+    } else {
+        let str = String(n);
+        let result = str.split('').reverse().join('');
+        return +result;
+    }
 }
